b8fbbb31efeb6155fd69cc32cc1e8daa9567e3d5
floyd <floyd.verjans@gmx.de>
2026-05-19 20:16:18 +0200

    init: skeleton

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'ffp-cache-v1';
+
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    'https://unpkg.com/html5-qrcode',
+    'https://unpkg.com/dexie@latest/dist/dexie.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+                return fetch(event.request);
+            })
+    );
+});
